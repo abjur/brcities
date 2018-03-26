756e45c130bad4ddc20e04ef2183901befc25db7
José de Jesus Filho <jjesusfilho@gmail.com>
2018-03-26 04:31:37 +0000

    add more details to sp_indicators function.

--- R/sp_indicators.R
+++ R/sp_indicators.R
@@ -8,14 +8,16 @@
 #'      variable name, period that the data is available, unity, year, and variable name.
 #'
 #' @details all codes for the places and indicators (variables) are available at `sp_place`
-#'      dataset.
+#'      dataset. You can provide more than one year, place or variable. If you want
+#'      either all years, all places or all variables, you can put "0" to one, and only
+#'      one, of the three arguments.
 #' @export
 #'
 #' @examples
 #'
 #' sp_indicators(2016:2017,place=1,variable=55)
 #'
-sp_indicators<-function(place,variable,year){
+sp_indicators<-function(year,place,variable){
 
   purrr::pmap_dfr(list(a=place,b=variable,c=year),function(a,b,c) {
 
